7f6109cbc68f2fedc2d0ed88856385e4eb1a7f49
IsoCreator <36802883+IsoCreator@users.noreply.github.com>
2018-05-23 00:38:28 +0200

    Add files via upload

---
+++ The second set of tasks/Zadanie2_1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.IOException;
+
+public class Zadanie2_1{
+public static void main(String args[])
+{
+	int n;
+	System.out.print("Ile razy");
+	Scanner liczba = new Scanner(System.in);
+	n=liczba.nextInt();
+	int i = 0;
+	
+	while(i<n){
+		System.out.println("JAVA");
+		i= i+1;
+	}
+}
+
+	
+}
